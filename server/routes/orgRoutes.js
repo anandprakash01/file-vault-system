28751c9331d7cb6889888cc1137e5dd88cdc06c3
anandprakash01 <ap.anandprakash21@gmail.com>
2026-05-28 11:05:12 +0530

    refactor: complete server rebuild

--- server/routes/orgRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const orgsController = require("../controllers/orgsController");
-
-const router = express.Router();
-
-router.get("/:orgId/files", orgsController.getFiles);
-
-module.exports = router;
